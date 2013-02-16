186e15a2f5688bffc47d56fb361b350382a8cce4
罗浩 <haozzzzzzzz@gmail.com>
2013-02-16 13:57:00 +0800

    Add Project

---
+++ WindowsPhone8_ConveyDataDemo/WindowsPhone8_ConveyDataDemo/SecondPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WindowsPhone8_ConveyDataDemo
+{
+    public partial class SecondPage : PhoneApplicationPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            this.PageTItle.Text=this.NavigationContext.QueryString["ConveyString"];
+        }
+    }
+}
